449ef37625e84ae7f05ca812bd426e618e349739
Jason Borum <33642393+jborum@users.noreply.github.com>
2018-03-25 20:16:37 -0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Excerise_5_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTerms = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.txtPI = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter # of Terms:";
+            // 
+            // txtTerms
+            // 
+            this.txtTerms.Location = new System.Drawing.Point(169, 25);
+            this.txtTerms.Name = "txtTerms";
+            this.txtTerms.Size = new System.Drawing.Size(375, 26);
+            this.txtTerms.TabIndex = 1;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(25, 215);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(536, 30);
+            this.buttonCalculate.TabIndex = 2;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // txtPI
+            // 
+            this.txtPI.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtPI.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPI.Font = new System.Drawing.Font("Swis721 Blk BT", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPI.ForeColor = System.Drawing.Color.Blue;
+            this.txtPI.Location = new System.Drawing.Point(25, 158);
+            this.txtPI.Name = "txtPI";
+            this.txtPI.Size = new System.Drawing.Size(536, 34);
+            this.txtPI.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(267, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Approximate value of pi after 0 terms";
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(25, 251);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(536, 33);
+            this.buttonClose.TabIndex = 5;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 301);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.txtPI);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.txtTerms);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Approximate PI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTerms;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox txtPI;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonClose;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excerise_5_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            int intTerms;
+
+            // Text box only allows integers
+            if (int.TryParse(txtTerms.Text, out intTerms))
+            {
+                // Declare variable
+                int index = 0;
+                double x = 4;
+                double y = 1;
+                double approx;
+                double newpi = 0.0;
+
+                // While loop to loop per number of integers selected by user
+                while (++index < intTerms - 1)
+                {
+                    //Algorithm to create = 4 - 4/3 + 4/5 - 4/6 + 4/7 - 4/8 + 4/9...
+                    approx = x / y;
+                    newpi = newpi + approx;
+                    y = y + 2;
+                    // Alternate the positive and negative value for x
+                    x = (x > 0) ? -4.0 : 4.0;
+                }
+
+                //Update text box and label
+                txtPI.Text = Convert.ToString(newpi);
+                this.label2.Text = "Approximate value of pi after " + txtTerms.Text + " terms";
+            }
+            else
+            {
+                //Message box to let user know there is an invalid input.
+                MessageBox.Show("Please Enter Valid Integer", "Input Error", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                txtTerms.Text = "";
+                this.ActiveControl = txtTerms;
+            }
+        }
+
+    }
+}
